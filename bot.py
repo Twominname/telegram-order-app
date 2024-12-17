013401ab7d3f6d8a473fb6acf2cba70ba95f528d
twominname <britvenkoe@gmail.com>
2024-12-17 13:38:27 +0200

    Обновлены страницы категорий и блюда

--- bot.py
+++ bot.py
@@ -1,17 +1,25 @@
 import telebot
 from telebot import types
 
-TOKEN = 'ваш_токен_бота'
-
+TOKEN = '7926473843:AAERqSOZ2U2hGPWPQIA6oSUl4kwQjpF1_vU'  # Замените на токен вашего бота
 bot = telebot.TeleBot(TOKEN)
 
+# Обработчик команды /start
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    # Создаем кнопку для запуска Web App
-    markup = types.ReplyKeyboardMarkup(row_width=1)
-    item = types.KeyboardButton("Заказать", web_app=types.WebAppInfo(url="https://your-web-app-url.com"))
-    markup.add(item)
+def start(message):
+    # Создаем инлайн кнопку с переходом на мини-приложение
+    markup = types.InlineKeyboardMarkup()
+    button = types.InlineKeyboardButton(
+        text="Перейти к меню", 
+        web_app=types.WebAppInfo(url="https://twominname.github.io/telegram-order-app/index.html")  # Замените на свой URL
+    )
+    markup.add(button)
+    bot.send_message(message.chat.id, "Привет! Нажми на кнопку, чтобы открыть меню и сделать заказ.", reply_markup=markup)
 
-    bot.send_message(message.chat.id, "Привет! Я твой заказной бот. Нажми кнопку для начала.", reply_markup=markup)
+# Обработчик всех сообщений (если нужно)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "Если ты хочешь сделать заказ, нажми на кнопку ниже.")
 
-bot.polling()
+# Запуск бота
+bot.polling(none_stop=True)
